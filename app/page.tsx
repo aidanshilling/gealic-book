2c335bb75a5b540ab219818b01efcae669ba5200
Aidan Shilling <aidanshilling91@gmail.com>
2023-07-10 12:05:27 +0100

    Added some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Hello, World!
+        The Lost Language
       </div> 
+      <div>
+        <ul>
+          <li>There are roughly 1.8 million people who claim to speak Irish.</li>
+          <li>The population of the Republic or Ireland is roughly 5 million persons.</li>
+          <li>That makes roughly 40% of the population.</li>
+        </ul>
+      </div>
     </main>
   )
 }
