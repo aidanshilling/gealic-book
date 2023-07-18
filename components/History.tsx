54dfd725c932acf824829886b4800b7e7d5bf29c
Aidan Shilling <aidanshilling91@gmail.com>
2023-07-18 19:43:38 +0100

    Added chart

--- components/History.tsx
+++ components/History.tsx
@@ -1,24 +1,28 @@
 "use client";
 import { Pie } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-	labels: ["Red", "Blue", "Yellow", "Green"],
+const data = {
+	labels: ["Spoken Very Well", "Spoken Well", "Spoken poorly"],
 	datasets: [
 		{
-			label: "# of Votes",
-			data: [12, 19, 3, 5],
-			backgroundColor: [
-				"rgba(255, 99, 132, 0.2)",
-				"rgba(54, 162, 235, 0.2)",
-				"rgba(255, 206, 86, 0.2)",
-				"rgba(75, 192, 192, 0.2)",
-			],
-			borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)"],
+			label: "% in speaking category",
+			data: [10, 32, 58],
+			backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+			borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
 			borderWidth: 1,
 		},
 	],
+};
+
+const options: ChartOptions = {
+	plugins: {
+		legend: {
+			position: "left",
+			align: "start",
+		},
+	},
 };
 
 const History = () => {
@@ -51,8 +55,9 @@ const History = () => {
 								preserving and promoting the language.
 							</p>
 						</div>
-						<div>
-							<Pie data={data} />
+						<div className="ml-8">
+							<h1 className="text-xl font-semibold">Ability to speak Irish, Census 2022</h1>
+							<Pie data={data} options={options} />
 						</div>
 					</div>
 				</div>
