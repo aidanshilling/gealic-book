2fe635a41690f6e0dc197c7dbac42063ece87032
Aidan Shilling <aidanshilling91@gmail.com>
2023-07-19 14:15:05 +0100

    Updated pj

--- components/Interview.tsx
+++ components/Interview.tsx
@@ -1,7 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
-import pj1 from "../public/pj1.jpg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
@@ -9,17 +8,19 @@ interface InterviewProps {
 	name: string;
 	videoUrl: string;
 	text: string;
+	image: StaticImageData;
 }
 
-const Interview = ({ name, videoUrl, text }: InterviewProps) => {
+const Interview = ({ image, name, videoUrl, text }: InterviewProps) => {
 	return (
 		<div className="mt-8">
 			<div className="grid grid-cols-2">
 				<div>
-					<Image src={pj1} alt={"PJ"} />
+					<Image src={image} alt={"PJ"} />
 				</div>
-				<div>
-					<p className="ml-8">{text}</p>
+				<div className="ml-8">
+					<h1 className="mb-8 text-3xl text-zinc-500 font-medium">{name}</h1>
+					<p>{text}</p>
 				</div>
 			</div>
 		</div>
