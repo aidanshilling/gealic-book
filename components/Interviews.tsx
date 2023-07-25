9038ea704dac5073e034b05c09b34ac198d2733f
Aidan Shilling <aidanshilling91@gmail.com>
2023-07-25 21:48:33 +0100

    Added bwpj

--- components/Interviews.tsx
+++ components/Interviews.tsx
@@ -1,5 +1,6 @@
 import Interview from "./Interview";
 import pj1 from "../public/pj1.jpg";
+import pjbw1 from "../public/pjbw1.jpg";
 import connor1 from "../public/connor1.jpg";
 import connor2 from "../public/connor2.jpg";
 import connor3 from "../public/connor3.jpg";
@@ -17,7 +18,7 @@ const Interviews = () => {
 				<div className="mx-auto max-w-4xl py-10">
 					<h1 className="font-bold text-5xl">People</h1>
 					<Interview
-						images={[pj1, sweenys1, sweenys2, sweenys3]}
+						images={[pj1, pjbw1, sweenys1, sweenys2, sweenys3]}
 						name="PJ"
 						text={`PJ is a remarkable individual who embodies a multitude of talents and passions. As the proud owner of Sweeny's Pharmacy, a cherished establishment nestled in the heart of Dublin, Ireland, PJ's shop serves as a captivating homage to the literary legacy of James Joyce and his seminal work, Ulysses. Not only is PJ an expert in the world of literature, but he is also a polyglot, fluent in an impressive array of ten languages, including French and Irish Gaelic. His linguistic prowess is matched only by his unwavering dedication as an advocate for the Irish Language, constantly striving to preserve and promote its rich cultural heritage. Beyond his literary and linguistic pursuits, PJ's passion extends to the realm of music, where he skillfully strums melodies on his guitar. Perhaps most notably, PJ's warm and welcoming nature shines through, as he consistently demonstrates an innate kindness that leaves a lasting impression on all those fortunate enough to cross his path.`}
 						videos={["https://youtu.be/8lgzWcKJpLs"]}
